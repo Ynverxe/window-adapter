771e738695edad9ad73fdc0372abef4fe8e27c58
Ynverxe <nahubar65@gmail.com>
2024-10-20 04:21:24 -0300

    ci: remove run-paper and gradlew.shadow plugins

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,13 +1,5 @@
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "8.3.0"
-    alias(libs.plugins.run.paper)
-}
-
-tasks {
-    runServer {
-        minecraftVersion("1.21")
-    }
 }
 
 repositories {
